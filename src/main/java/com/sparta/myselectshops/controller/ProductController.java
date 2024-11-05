ac1ff111e6cbb66c96383f1dd75471588ee34574
eleunadeu <eleunadeu@gmail.com>
2024-11-05 12:05:57 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshops/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshops/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.sparta.myselectshops.controller;
 import com.sparta.myselectshops.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshops.dto.ProductRequestDto;
 import com.sparta.myselectshops.dto.ProductResponseDto;
+import com.sparta.myselectshops.security.UserDetailsImpl;
 import com.sparta.myselectshops.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +20,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-        return productService.createProduct(requestDto);
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.createProduct(requestDto, userDetails.getUser());
     }
 
     @PutMapping("/products/{id}")
@@ -27,7 +30,12 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts() {
-        return productService.getProducts();
+    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.getProducts(userDetails.getUser());
+    }
+
+    @GetMapping("/admin/products")
+    public List<ProductResponseDto> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/sparta/myselectshops/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshops/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.myselectshops.repository;
 
 import com.sparta.myselectshops.entity.Product;
+import com.sparta.myselectshops.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUser(User user);
 }
