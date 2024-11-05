3a2bcd8f298475e55e2d92dd3cf157ee2965a43d
eleunadeu <eleunadeu@gmail.com>
2024-11-05 10:44:10 +0900

    init commit

---
+++ src/main/java/com/sparta/myselectshops/MyselectshopsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselectshopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselectshopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/myselectshops/MyselectshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.myselectshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselectshopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
