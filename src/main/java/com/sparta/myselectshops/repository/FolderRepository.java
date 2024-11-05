400f32fe4e7b3a99c7cbf3947c7e1b6d5bf8559b
eleunadeu <eleunadeu@gmail.com>
2024-11-05 12:31:06 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshops/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.myselectshops.repository;
+
+import com.sparta.myselectshops.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
---
+++ src/main/java/com/sparta/myselectshops/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.myselectshops.repository;
+
+import com.sparta.myselectshops.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
